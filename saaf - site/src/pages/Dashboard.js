73bf5c37335d14e1a17fcf6bf48af7dfd693c3b8
Jhonatan Borges <jhonatantecno23@gmail.com>
2023-07-07 19:07:53 -0300

    Background added

--- saaf - site/src/pages/Dashboard.js
+++ saaf - site/src/pages/Dashboard.js
@@ -2,41 +2,69 @@ import NivelAgua from "./NivelAgua"
 import NivelTurbidez from "./NivelTurbidez"
 import styles from '../styles/Dashboard.module.css'
 import { useEffect, useState } from "react"
-import { getDatabase, ref, onValue, update} from "firebase/database";
+import { getDatabase, ref, onValue, update } from "firebase/database";
 
 
 export default function Dashboard() {
 
-    const [estadoBomba, setEstadoBomba] = useState(false);
+  const [estadoBomba, setEstadoBomba] = useState(false);
 
-    useEffect(() => {
-        const estadoBombaRef = ref(getDatabase());
-        update(estadoBombaRef, {estado_bomba: estadoBomba})
+  useEffect(() => {
+    const estadoBombaRef = ref(getDatabase());
+    update(estadoBombaRef, { estado_bomba: estadoBomba })
+    console.log("TESTE")
 
-    }, [estadoBomba]);
+  }, [estadoBomba]);
+
+  useEffect(() => {
+    const estadoBombaRef = ref(getDatabase(), '/estado_bomba/');
+    onValue(estadoBombaRef, (snapshot) => {
+      const snap = snapshot.val();
+      if(snapshot.exists()) {
+        setEstadoBomba(snap);
+        console.log(snap);
+
+        return;
+      };
+      
+      
+    });
+
+  }, []);
+
+
+
+
   return (<>
-  <div className={styles.nav}>
-    <div>
+    <div className={styles.nav}>
+      <div>
         <h2>SAA - Familiar</h2>
+      </div>
+      <h2>DASHBOARD</h2>
     </div>
-    <h2>DASBOARD</h2>
-  </div>
-  <div className={styles.body}>
-    <div className={styles.info}>
+    <div className={styles.body}>
+      <div className={styles.info}>
 
-    <div className={styles.nivelagua}>
+        <div className={styles.nivelagua}>
 
-    <NivelAgua />
-    </div>
-    <div className={styles.nivelturbidez}>
-    <button onClick={() => setEstadoBomba(!estadoBomba)}>LIGAR A BOMBA</button>
-    <NivelTurbidez />
+          <NivelAgua />
+        </div>
+        <div className={styles.grafico2}>
 
+
+          {estadoBomba ? <button className={styles.botaoDesliga} onClick={() => setEstadoBomba(!estadoBomba)}>DESLIGAR A BOMBA</button>
+            : <button className={styles.botaoLiga} onClick={() => setEstadoBomba(!estadoBomba)}>LIGAR A BOMBA</button>
+          }
+          
+          <div className={styles.nivelturbidez}>
+            {estadoBomba ? <NivelTurbidez /> : <h2>A bomba esta desligada</h2>}
+          </div>
+
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-    {console.log(estadoBomba)}
-    
-    </>
+
+
+  </>
   )
 }
